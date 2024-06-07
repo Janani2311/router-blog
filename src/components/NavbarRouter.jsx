d5760d6e24bac21301191fb030b87c1407b8ded1
janani2311 <janaeswar@gmail.com>
2024-06-07 20:04:17 +0530

    css changes

--- src/components/NavbarRouter.jsx
+++ src/components/NavbarRouter.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -10,8 +11,8 @@ function NavbarRouter() {
         <Navbar expand="md" bg="light" data-bs-theme="light">
         <Container>
           <Navbar.Brand className='logo'>GUVI</Navbar.Brand>
-          <Nav className="me-auto" variant="underline">  
-          <Link to='/' className="nav-link">All</Link>
+          <Nav className="me-auto" variant="underline" activeKey="/">  
+          <Link to='/' className="nav-link">ALL</Link>
           <Link to='/full-stack' className="nav-link">FULL STACK DEVELOPMENT</Link>
           <Link to='/data-science' className="nav-link">DATASCIENCE</Link>
           <Link to='/cyber-security' className="nav-link">CYBER SECURITY</Link>
--- src/components/Showcard.jsx
+++ src/components/Showcard.jsx
@@ -5,15 +5,17 @@ function Showcard({data}) {
   return <>
     {
         data.map((e,i)=>{
-            return <Card style={{ width: '18rem' }} key={i}>
-                <Card.Img variant="top" src={e.image} />
-                <Card.Body>
-                    <Card.Title>{e.title}</Card.Title>
-                </Card.Body>
-            </Card>
+            return <Card style={{ width: '18rem', height: '300px' }} key={i}>
+                  <Card.Img variant="top" className="card-img" src={e.image} />
+                  <Card.Body>
+                      <Card.Title className='card-title'>{e.title}</Card.Title>
+                  </Card.Body>
+                </Card>
+             
+               
         })
         
-    }   
+    } 
   </>
 }
 
